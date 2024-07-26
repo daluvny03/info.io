cb30b53f04064ab80ab84c54a61458bc55284c07
daluvny <daluvny03@gmail.com>
2024-07-26 10:22:58 +0700

    Add files via upload

---
+++ register.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDAeXTiTB5WnEoxlFdub0IYydNaIOU_QmY",
+    authDomain: "login-aza.firebaseapp.com",
+    projectId: "login-aza",
+    storageBucket: "login-aza.appspot.com",
+    messagingSenderId: "175360617845",
+    appId: "1:175360617845:web:84f334f785322ca022f73c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
